8bf8ae340fd1c4f58548c9df52ed3d1547c40b88
David Garske <david@wolfssl.com>
2018-02-06 16:27:50 -0800

    wolfTPM v1.0 TPM 2.0 support: * Support for all TPM2 API's using TIS and SPI IO callback. * Helper for getting TPM return code string `TPM2_GetRCString`. * TPM 2.0 demo code in `examples/tpm/tpm2_demo.c` with support for STM32 CubeMX SPI as reference.
    Requires wolfSSL (https://github.com/wolfSSL/wolfssl/pull/1344)

---
+++ IDE/OPENSTM32/Inc/wolftpm_example.h
@@ -0,0 +1,47 @@
+/* wolftpm_example.h
+ *
+ * Copyright (C) 2006-2017 wolfSSL Inc.
+ *
+ * This file is part of wolfSSL. (formerly known as CyaSSL)
+ *
+ * wolfTPM is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * wolfTPM is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA
+ */
+
+
+#ifndef WOLFTPM_EXAMPLE_H_
+#define WOLFTPM_EXAMPLE_H_
+
+#include <stm32f4xx_hal.h>
+#include <stm32f4xx.h>
+#include <cmsis_os.h>
+
+#ifdef HAVE_CONFIG_H
+    #include <config.h>
+#endif
+
+#ifndef WOLFSSL_USER_SETTINGS
+	#include <wolfssl/options.h>
+#endif
+#include <wolfssl/wolfcrypt/settings.h>
+#include <wolfssl/ssl.h>
+#include <wolfcrypt/test/test.h>
+#include <wolfcrypt/benchmark/benchmark.h>
+
+#include <wolftpm/tpm2.h>
+
+void wolfTPMDemo(void const * argument);
+
+
+#endif /* WOLFTPM_EXAMPLE_H_ */
---
+++ IDE/OPENSTM32/Src/wolftpm_example.c
@@ -0,0 +1,123 @@
+/* wolftpm_example.c
+ *
+ * Copyright (C) 2006-2017 wolfSSL Inc.
+ *
+ * This file is part of wolfSSL. (formerly known as CyaSSL)
+ *
+ * wolfTPM is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * wolfTPM is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA
+ */
+
+
+#include "wolfssl_example.h"
+
+/*****************************************************************************
+ * Private types/enumerations/variables
+ ****************************************************************************/
+
+/* UART definitions */
+extern UART_HandleTypeDef huart4;
+extern SPI_HandleTypeDef hspi1;
+
+
+#ifdef WOLF_TPM2
+extern int TPM2_Demo(void);
+#endif
+
+
+/*****************************************************************************
+ * Public types/enumerations/variables
+ ****************************************************************************/
+typedef struct func_args {
+    int    argc;
+    char** argv;
+    int    return_code;
+} func_args;
+
+const char menu1[] = "\r\n"
+    "\tt. WolfCrypt Test\r\n"
+    "\tb. WolfCrypt Benchmark\r\n"
+	"\tm. WolfCrypt TPM 2.0 Test\r\n";
+
+/*****************************************************************************
+ * Private functions
+ ****************************************************************************/
+
+
+/*****************************************************************************
+ * Public functions
+ ****************************************************************************/
+void wolfCryptDemo(void const * argument)
+{
+    uint8_t buffer[2];
+    func_args args;
+
+    while (1) {
+        printf("\r\n\t\t\t\tMENU\r\n");
+        printf(menu1);
+        printf("Please select one of the above options: ");
+
+        HAL_UART_Receive(&huart4, buffer, sizeof(buffer), 5000);
+
+        switch (buffer[0]) {
+
+        case 't':
+            memset(&args, 0, sizeof(args));
+            printf("\nCrypt Test\n");
+            wolfcrypt_test(&args);
+            printf("Crypt Test: Return code %d\n", args.return_code);
+            break;
+
+        case 'b':
+            memset(&args, 0, sizeof(args));
+            printf("\nBenchmark Test\n");
+            benchmark_test(&args);
+            printf("Benchmark Test: Return code %d\n", args.return_code);
+            break;
+
+        case 'm':
+			printf("\nTPM 2.0 Test\n");
+#ifdef WOLF_TPM2
+			args.return_code = TPM2_Demo();
+#endif
+			printf("TPM 2.0 Test: Return code %d\n", args.return_code);
+			break;
+
+        // All other cases go here
+        default: printf("\r\nSelection out of range\r\n"); break;
+        }
+    }
+}
+
+extern RTC_HandleTypeDef hrtc;
+double current_time()
+{
+	RTC_TimeTypeDef time;
+	RTC_DateTypeDef date;
+	uint32_t subsec;
+
+	/* must get time and date here due to STM32 HW bug */
+	HAL_RTC_GetTime(&hrtc, &time, FORMAT_BIN);
+	HAL_RTC_GetDate(&hrtc, &date, FORMAT_BIN);
+	subsec = (255 - time.SubSeconds) * 1000 / 255;
+
+	(void)date;
+
+	/* return seconds.milliseconds */
+	return ((double)time.Hours * 24) +
+		   ((double)time.Minutes * 60) +
+			(double)time.Seconds +
+		   ((double)subsec/1000);
+}
+
