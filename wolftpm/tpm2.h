6f89ce5f15057f993a1b30415a0f914de8fa4140
Dimitar Tomov <dimi@designfirst.ee>
2020-05-27 00:21:27 +0300

    Add missing overload in TPMU_SYM_KEY_BITS
    As defined in the Part 2 Structures of the TCG TPM2.0 specification
    
    Signed-off-by: Dimitar Tomov <dimi@designfirst.ee>

--- wolftpm/tpm2.h
+++ wolftpm/tpm2.h
@@ -1080,6 +1080,7 @@ typedef TPM_KEY_BITS TPMI_AES_KEY_BITS;
 typedef union TPMU_SYM_KEY_BITS {
     TPMI_AES_KEY_BITS aes;
     TPM_KEY_BITS sym;
+    TPMI_ALG_HASH xor;
 } TPMU_SYM_KEY_BITS;
 
 typedef union TPMU_SYM_MODE {
