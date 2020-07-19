2de6ff80cbd2d929d30fab0d5ec29ac1ffca3bdf
Dimitar Tomov <dimi@designfirst.ee>
2020-07-19 14:56:04 +0300

    Add pcr extend helper tool to compliment the TPM2 Quote example
    Signed-off-by: Dimitar Tomov <dimi@designfirst.ee>

---
+++ examples/pcr/extend.c
@@ -0,0 +1,170 @@
+/* extend.c
+ *
+ * Copyright (C) 2006-2020 wolfSSL Inc.
+ *
+ * This file is part of wolfTPM.
+ *
+ * wolfTPM is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * wolfTPM is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA
+ */
+
+/* This is a helper tool for extending hash into a TPM2.0 PCR */
+
+#include <wolftpm/tpm2_wrap.h>
+
+#include <examples/pcr/extend.h>
+#include <examples/tpm_io.h>
+#include <examples/tpm_test.h>
+
+#include <stdio.h>
+
+
+/******************************************************************************/
+/* --- BEGIN TPM2.0 PCR Extend example tool  -- */
+/******************************************************************************/
+
+inline static void usage(void)
+{
+    printf("Expected usage:\n");
+    printf("extend (pcr_number) (filename)\n");
+    printf("* pcr_number is a PCR index between 0-23\n");
+    printf("* filename points to file(data) to measure\n");
+    printf("Demo usage without parameters, extends PCR16 with known hash.\n");
+}
+
+int TPM2_Extend_Test(void* userCtx, int argc, char *argv[])
+{
+    int i, pcrIndex, rc = -1;
+    const char *filename = NULL;
+    FILE *dataFile = NULL;
+    WOLFTPM2_DEV dev;
+
+    union {
+        PCR_Extend_In pcrExtend;
+        PCR_Read_In pcrRead;
+        FlushContext_In flushCtx;
+        byte maxInput[MAX_COMMAND_SIZE];
+    } cmdIn;
+    union {
+        PCR_Read_Out pcrRead;
+        byte maxOutput[MAX_RESPONSE_SIZE];
+    } cmdOut;
+
+    TPM_HANDLE sessionHandle = TPM_RH_NULL;
+    TPMS_AUTH_COMMAND session[MAX_SESSION_NUM];
+
+    if(argc == 3) {
+        pcrIndex = strtol(argv[1], NULL, 10);
+        if(pcrIndex < 0 || pcrIndex > 23) {
+            printf("PCR index is out of range(0-23)\n");
+            goto exit;
+        }
+
+        filename = argv[2];
+        dataFile = fopen(filename, "rb");
+        if(dataFile == NULL) {
+            printf("Error openning file %s\n", filename);
+            usage();
+            goto exit;
+        }
+    }
+    else if(argc == 1) {
+        pcrIndex = 16; /* PCR16 is for DEBUG purposes, thus safe to use */
+    }
+    else {
+        printf("Incorrect arguments\n");
+        usage();
+        goto exit;
+    }
+
+    printf("Extending hash into a PCR (TPM2.0 measurement)\n");
+    rc = wolfTPM2_Init(&dev, TPM2_IoCb, userCtx);
+    if (rc != TPM_RC_SUCCESS) {
+        printf("wolfTPM2_Init failed 0x%x: %s\n", rc, TPM2_GetRCString(rc));
+        goto exit;
+    }
+    printf("wolfTPM2_Init: success\n");
+
+
+    /* Define the default session auth that has NULL password */
+    XMEMSET(session, 0, sizeof(session));
+    session[0].sessionHandle = TPM_RS_PW;
+    session[0].auth.size = 0; /* NULL Password */
+    TPM2_SetSessionAuth(session);
+
+    /* PCR Extend */
+    XMEMSET(&cmdIn.pcrExtend, 0, sizeof(cmdIn.pcrExtend));
+    cmdIn.pcrExtend.pcrHandle = pcrIndex;
+    cmdIn.pcrExtend.digests.count = 1;
+    cmdIn.pcrExtend.digests.digests[0].hashAlg = TPM_ALG_SHA256;
+    for (i=0; i<TPM_SHA256_DIGEST_SIZE; i++) {
+        cmdIn.pcrExtend.digests.digests[0].digest.H[i] = i;
+    }
+    rc = TPM2_PCR_Extend(&cmdIn.pcrExtend);
+    if (rc != TPM_RC_SUCCESS) {
+        printf("TPM2_PCR_Extend failed 0x%x: %s\n", rc,
+            TPM2_GetRCString(rc));
+        goto exit;
+    }
+    printf("TPM2_PCR_Extend success\n");
+
+    XMEMSET(&cmdIn.pcrRead, 0, sizeof(cmdIn.pcrRead));
+    TPM2_SetupPCRSel(&cmdIn.pcrRead.pcrSelectionIn,
+        TEST_WRAP_DIGEST, pcrIndex);
+    rc = TPM2_PCR_Read(&cmdIn.pcrRead, &cmdOut.pcrRead);
+    if (rc != TPM_RC_SUCCESS) {
+        printf("TPM2_PCR_Read failed 0x%x: %s\n", rc, TPM2_GetRCString(rc));
+        goto exit;
+    }
+    printf("TPM2_PCR_Read: Index %d, Count %d\n",
+            pcrIndex, (int)cmdOut.pcrRead.pcrValues.count);
+    if (cmdOut.pcrRead.pcrValues.count > 0) {
+        printf("TPM2_PCR_Read: Index %d, Digest Sz %d, Update Counter %d\n",
+            pcrIndex,
+            (int)cmdOut.pcrRead.pcrValues.digests[0].size,
+            (int)cmdOut.pcrRead.pcrUpdateCounter);
+        TPM2_PrintBin(cmdOut.pcrRead.pcrValues.digests[0].buffer,
+                      cmdOut.pcrRead.pcrValues.digests[0].size);
+    }
+
+exit:
+
+    if (dataFile != NULL) {
+        fclose(dataFile);
+    }
+    /* Close session */
+    if (sessionHandle != TPM_RH_NULL) {
+        cmdIn.flushCtx.flushHandle = sessionHandle;
+        TPM2_FlushContext(&cmdIn.flushCtx);
+    }
+
+    wolfTPM2_Cleanup(&dev);
+    return rc;
+}
+
+/******************************************************************************/
+/* --- END TPM2.0 PCR Extend example tool -- */
+/******************************************************************************/
+
+
+#ifndef NO_MAIN_DRIVER
+int main(int argc, char *argv[])
+{
+    int rc;
+
+    rc = TPM2_Extend_Test(NULL, argc, argv);
+
+    return rc;
+}
+#endif
