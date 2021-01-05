6687d7b91e1a1ef1a99ab829528ff1f93fefaf1a
Dimitar Tomov <dimi@wolfssl.com>
2021-01-05 15:24:39 +0200

    Added verification of the USB2SPI bridge
    Signed-off-by: Dimitar Tomov <dimi@wolfssl.com>

--- src/tpm2_cyusb.c
+++ src/tpm2_cyusb.c
@@ -24,30 +24,91 @@
 #include <wolftpm/tpm2_cyusb.h>
 #include <wolftpm/tpm2_packet.h>
 #include <wolftpm/tpm2_wrap.h>
-/* TODO figure out instructions for acquiring Cypress libusb */
+/* TODO figure out a good way for acquiring and including the Cypress libusb */
 #include "../usb2go/cylib/common/header/CyUSBSerial.h"
 
-
-/* Support a single TPM using Cypress UBS */
-#ifndef TPM2_CYUSB_NUMBER
-#define TPM2_CYUSB_NUMBER 0
-#endif
 
 /* LetsTrust USB2GO TPM2.0 stick has SPI as interface zero(0) */
 #ifndef TPM2_CYUSB_INTERFACE
 #define TPM2_CYUSB_INTERFACE 0
 #endif
 
+#define TPM2_CYUSB_RC_NODEVICE (-1)
+#define TPM2_CYUSB_SIGNATURE_LENGTH 6
+
+/* Helper function to check Cypress magic value(signature */
+static int TPM2_CYUSB_VerifySignature(int deviceNumber)
+{
+    int rc = FALSE;
+    CY_HANDLE handle;
+    CY_RETURN_STATUS rStatus;
+    BYTE signature[TPM2_CYUSB_SIGNATURE_LENGTH];
+
+    rStatus = CyOpen(deviceNumber, TPM2_CYUSB_INTERFACE, &handle);
+    if (rStatus == CY_SUCCESS) {
+        rStatus = CyGetSignature(handle, signature);
+
+        rc = (rStatus == CY_SUCCESS) ? TRUE : FALSE;
+
+        CyClose (handle);
+    }
+
+    return rc;
+}
+
+
+/* Helper function to find a Cypress USB2SPI device
+ *
+ * Matches Cypress signature, device class and interface type
+ *
+ * Return value
+ * On success, the number of the device on the UBS bus
+ * On failure, TPM2_CYUSB_RC_NODEVICE is returned
+ */
+static int TPM2_CYUSB_FindDevice(int* deviceNumber) {
+    int devNum = TPM2_CYUSB_RC_NODEVICE;
+    BYTE deviceCount;
+    CY_DEVICE_INFO deviceInfo;
+    CY_RETURN_STATUS rStatus;
+
+    rStatus = CyGetListofDevices(&deviceCount);
+    if (rStatus != CY_SUCCESS) {
+        printf("CYUSB: Unable to get the list of devices ERRNO=%d\n", rStatus);
+        return TPM_RC_FAILURE;
+    }
+
+#ifdef DEBUG_WOLFTPM
+     printf("The first USB2SPI device found is tried as TPM2.0 on USB\n");
+#endif
+
+    for(devNum=0; devNum < deviceCount; devNum++) {
+        rStatus = CyGetDeviceInfo (devNum, &deviceInfo);
+        if (!rStatus) {
+            /* Verify magic Cypress value(signature) */
+            if (!TPM2_CYUSB_VerifySignature(devNum)) {
+                continue;
+            }
+            /* Verify this is indeed a Cypress UBS bridge for SPI */
+            if (deviceInfo.deviceClass[TPM2_CYUSB_INTERFACE] == CY_CLASS_VENDOR) {
+                if(deviceInfo.deviceType[TPM2_CYUSB_INTERFACE] == CY_TYPE_SPI) {
+                    break;
+                }
+            }
+        }
+    }
+
+    *deviceNumber = devNum;
+    return TPM_RC_SUCCESS;
+}
+
 /* Talk to a TPM device through the Cypress USB2SPI converter and libusb */
 int TPM2_CYUSB_SendCommand(TPM2_CTX* ctx, TPM2_Packet* packet)
 {
     int rc = TPM_RC_FAILURE;
-    BYTE numberOfDevices;
     CY_RETURN_STATUS rStatus;
     CY_HANDLE handle;
     CY_DATA_BUFFER writeBuf, readBuf;
-    int deviceNumber = TPM2_CYUSB_NUMBER;
-    int interfaceNumber=TPM2_CYUSB_INTERFACE;
+    int deviceNumber;
 
 #ifdef WOLFTPM_DEBUG_VERBOSE
     printf("Command size: %d\n", packet->pos);
@@ -57,22 +118,19 @@ int TPM2_CYUSB_SendCommand(TPM2_CTX* ctx, TPM2_Packet* packet)
     rStatus = CyLibraryInit();
     if (rStatus != CY_SUCCESS) {
         printf("CYUSB: Cypress USB library init failed ERRNO=%d\n", rStatus);
+        return rc;
+    }
+
+    rc = TPM2_CYUSB_FindDevice(&deviceNumber);
+    if (rc != TPM_RC_SUCCESS || deviceNumber == TPM2_CYUSB_RC_NODEVICE) {
+        printf("No matching UBS device found\n");
     }
     else {
-        rStatus = CyGetListofDevices(&numberOfDevices);
+        rStatus = CyOpen(deviceNumber, TPM2_CYUSB_INTERFACE, &handle);
         if (rStatus != CY_SUCCESS) {
-            printf("CYUSB: Unable to get the list of devices ERRNO=%d\n", rStatus);
-        }
-        else if (numberOfDevices > 1) {
-            printf("wolfTPM: Multiple TPM USB devices detected\n");
-            printf("wolfTPM: Leave only one TPM USB stick and try again.\n");
+            printf("CYUSB: Unable to open the UBS device ERRNO=%d\n", rStatus);
         }
         else {
-            rStatus = CyOpen(deviceNumber, interfaceNumber, &handle);
-            if (rStatus != CY_SUCCESS) {
-                printf("CYUSB: Unable to open the UBS device ERRNO=%d\n", rStatus);
-            }
-
             writeBuf.length = packet->pos;
             writeBuf.buffer = packet->buf;
             /* TODO: Confirm the Cypress library can safely use one buffer */
@@ -87,10 +145,11 @@ int TPM2_CYUSB_SendCommand(TPM2_CTX* ctx, TPM2_Packet* packet)
                packet->pos = readBuf.transferCount;
                rc = TPM_RC_SUCCESS;
             }
-
             CyClose(handle);
         }
     }
+
+    CyLibraryExit();
 
 #ifdef WOLFTPM_DEBUG_VERBOSE
     if (rspSz > 0) {
