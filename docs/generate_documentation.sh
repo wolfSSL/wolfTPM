9b6791657bf8c3e3f738ffd4a931f7a48d6376a4
David Garske <david@wolfssl.com>
2021-06-01 09:42:59 -0700

    Fixes to support "make dox".

--- docs/generate_documentation.sh
+++ docs/generate_documentation.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 
+CURRDIR=${PWD##*/}
+if [ "$CURRDIR" != "docs" ]; then
+    echo "Please run from the ./docs directory"
+    exit 1
+fi
+
+# Run from ./docs
 echo "Generating html..."
 doxygen Doxyfile
 echo "Finished generating html..."
 
-echo "To view the html files use a browser to open the index.html file located at doc/html/index.html"
+echo "To view the html files use a browser to open the index.html file located at docs/html/index.html"
