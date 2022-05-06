ed70aeb7030bae24d362af574a8514fc2abdca4a
Elms <jeff@wolfssl.com>
2022-05-06 10:51:29 -0700

    v2.4.0 release preparation

--- wolftpm/version.h
+++ wolftpm/version.h
@@ -34,8 +34,8 @@
 extern "C" {
 #endif
 
-#define LIBWOLFTPM_VERSION_STRING "2.3.1"
-#define LIBWOLFTPM_VERSION_HEX 0x02003001
+#define LIBWOLFTPM_VERSION_STRING "2.4.0"
+#define LIBWOLFTPM_VERSION_HEX 0x02004000
 
 #ifdef __cplusplus
 }
