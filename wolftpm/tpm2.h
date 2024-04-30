38f0ac555d5aeab55da41e697d8f6f753e787b48
David Garske <david@wolfssl.com>
2024-04-30 13:55:44 -0700

    Added TPM simulator support for UART transport (`--enable-swtpm=uart`). Refactor debug printf to new macro to support Xilinx FSBL.

--- wolftpm/tpm2.h
+++ wolftpm/tpm2.h
@@ -1803,8 +1803,18 @@ static const BYTE TPM_20_IAK_POLICY_SHA512[] = {
 struct TPM2_CTX;
 
 #ifdef WOLFTPM_SWTPM
+#if defined(WOLFTPM_SWTPM_UARTNS550)
+    #include "xparameters.h"
+    #include "xuartns550.h"
+#endif
+
 struct wolfTPM_tcpContext {
+#if defined(WOLFTPM_SWTPM_UARTNS550)
+    XUartNs550 fd;
+    int setup;
+#else
     int fd;
+#endif
 };
 #endif /* WOLFTPM_SWTPM */
 
