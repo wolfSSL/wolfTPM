a40ad7b8f08207e4e53d5ffe0f5190833f75edac
aidan garske <aidangarske03@gmail.com>
2025-01-02 16:28:13 -0800

    wolfTPM Zephyr Support initial support and tests for bench and unit_test coverage tpm. Tested on zephyr and gets moved over in enviroment

---
+++ zephyr/samples/wolftpm_bench/install_bench.sh
@@ -0,0 +1,49 @@
+#!/bin/sh
+
+WOLFTPM_SRC_DIR=../../../..
+
+if [ ! -d $WOLFTPM_SRC_DIR ]; then
+    echo "Directory does not exist: $WOLFTPM_SRC_DIR"
+    exit 1
+fi
+if [ ! -f $WOLFTPM_SRC_DIR/examples/bench/bench.c ]; then
+    echo "Missing source file: $WOLFTPM_SRC_DIR/examples/bench/bench.h"
+    exit 1
+fi
+
+ZEPHYR_DIR=
+if [ $# -ne 1 ]; then
+    echo "Need location of zephyr project as a command line argument"
+    exit 1
+else
+    ZEPHYR_DIR=$1
+fi
+if [ ! -d $ZEPHR_DIR ]; then
+    echo "Zephyr project directory does not exist: $ZEPHYR_DIR"
+    exit 1
+fi
+ZEPHYR_SAMPLES_DIR=$ZEPHYR_DIR/zephyr/samples/modules
+if [ ! -d $ZEPHYR_SAMPLES_DIR ]; then
+    echo "Zephyr samples/modules directory does not exist: $ZEPHYR_SAMPLES_DIR"
+    exit 1
+fi
+ZEPHYR_WOLFTPM_DIR=$ZEPHYR_SAMPLES_DIR/wolftpm_bench
+
+echo "wolfSSL directory:"
+echo "  $ZEPHYR_WOLFTPM_DIR"
+rm -rf $ZEPHYR_WOLFTPM_DIR
+mkdir $ZEPHYR_WOLFTPM_DIR
+
+echo "Copy in Build files ..."
+cp -r * $ZEPHYR_WOLFTPM_DIR/
+rm $ZEPHYR_WOLFTPM_DIR/$0
+
+echo "Copy Source Code ..."
+rm -rf $ZEPHYR_WOLFTPM_DIR/src
+mkdir $ZEPHYR_WOLFTPM_DIR/src
+
+cp -rf ${WOLFTPM_SRC_DIR}/examples/bench/bench.c $ZEPHYR_WOLFTPM_DIR/src/
+cp -rf ${WOLFTPM_SRC_DIR}/examples/bench/bench.h $ZEPHYR_WOLFTPM_DIR/src/
+
+echo "Done"
+
