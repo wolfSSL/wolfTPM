217bcbd953f7eee32369b1ad3c11f20e3fd0f9c0
aidan garske <aidangarske03@gmail.com>
2025-01-22 20:55:09 -0800

    update user settings to have the tpm template and added a unified settings

--- zephyr/user_settings.h
+++ zephyr/user_settings.h
@@ -2,14 +2,14 @@
  *
  * Copyright (C) 2006-2024 wolfSSL Inc.
  *
- * This file is part of wolfTPM.
+ * This file is part of wolfSSL.
  *
- * wolfTPM is free software; you can redistribute it and/or modify
+ * wolfSSL is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation; either version 2 of the License, or
  * (at your option) any later version.
  *
- * wolfTPM is distributed in the hope that it will be useful,
+ * wolfSSL is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
@@ -19,6 +19,25 @@
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1335, USA
  */
 
+/* Options for USE_LOW_RESOURCE generated with configure and wolfssl/options.h
+ * using:
+./configure --enable-lowresource --enable-cryptonly --enable-aescfb \
+  --enable-hmac --enable-cryptocb --enable-singlethreaded \
+  --enable-sp=smallrsa2048 --enable-sp-math --disable-sp-asm \
+  --disable-filesystem --disable-asm --disable-pkcs8 --disable-pkcs12 \
+  --disable-ecc --disable-dh \
+  --disable-sha224 --disable-sha384 --disable-sha512 \
+  --disable-md5 --disable-sha3 \
+  --disable-aescbc --disable-aesgcm --disable-aesccm \
+  --disable-poly1305 --disable-chacha \
+  --disable-hashdrbg \
+  --disable-sys-ca-certs --disable-error-queue-per-thread \
+  --disable-oldtls --disable-errorstrings --disable-memory --disable-coding \
+  --disable-examples --disable-optflags \
+  CFLAGS="-Os -DNO_SIG_WRAPPER -DNO_AES_192 -DNO_AES_256 \
+  -DWOLFSSL_RSA_PUBLIC_ONLY -DWOLFSSL_RSA_VERIFY_INLINE -DNO_CHECK_PRIVATE_KEY"
+ */
+
 #ifndef USER_SETTINGS_H
 #define USER_SETTINGS_H
 
@@ -26,18 +45,10 @@
 extern "C" {
 #endif
 
-/* WOLFTPM ZEPHYR SETTINGS */
+/* -- WOLFTPM ZEPHYR SETTINGS -- */
 
 #undef WOLFTPM_USER_SETTINGS
 #define WOLFTPM_USER_SETTINGS
-
-/*
-#undef WOLFTPM_SWTPM
-#define WOLFTPM_SWTPM
-*/
-
-#undef WOLF_CRYPT_SETTINGS_H
-#define WOLF_CRYPT_SETTINGS_H
 
 #undef WOLFTPM_EXAMPLE_HAL
 #define WOLFTPM_EXAMPLE_HAL
@@ -45,109 +56,179 @@ extern "C" {
 #undef  WOLFTPM_ZEPHYR
 #define WOLFTPM_ZEPHYR
 
-/* WOLFSSL SETTINGS */
+/* -- WOLFSSL SETTINGS -- */
 
 #undef WOLFSSL_USER_SETTINGS
 #define WOLFSSL_USER_SETTINGS
 
-#undef  TFM_TIMING_RESISTANT
+#undef WOLF_CRYPT_SETTINGS_H
+#define WOLF_CRYPT_SETTINGS_H
+
+#if 0
+    #undef WOLFTPM_SWTPM
+    #define WOLFTPM_SWTPM
+#endif
+
+#undef WOLF_CRYPTO_CB
+#define WOLF_CRYPTO_CB
+
+#undef WOLFSSL_AES_CFB
+#define WOLFSSL_AES_CFB
+
+/* -- WOLFTPM SETTINGS -- */
+
+/* enable for low resource options */
+#if 0
+    #define USE_LOW_RESOURCE
+#endif
+
+#ifdef USE_LOW_RESOURCE
+    /* wolfCrypt only (no SSL/TLS) */
+    #define WOLFCRYPT_ONLY
+#else
+    /* wolfTPM with TLS example (v1.3 only) */
+    #define WOLFSSL_TLS13
+    #define WOLFSSL_NO_TLS12
+    #define NO_OLD_TLS
+
+    #define HAVE_TLS_EXTENSIONS
+    #define HAVE_SUPPORTED_CURVES
+    #define HAVE_SERVER_RENEGOTIATION_INFO
+    #define HAVE_ENCRYPT_THEN_MAC
+
+    #define HAVE_HKDF
+    #define WC_RSA_PSS
+    #define WOLFSSL_PSS_LONG_SALT
+#endif
+
+/* No threading or file system */
+#define SINGLE_THREADED
+
+/* Enable crypto callbacks */
+#define WOLF_CRYPTO_CB
+
+#ifdef USE_LOW_RESOURCE
+    /* Single Precision math for RSA 2048 only (small) */
+    #define WOLFSSL_HAVE_SP_RSA
+    #define WOLFSSL_SP_MATH
+    #define WOLFSSL_SP_SMALL
+    #define WOLFSSL_SP_NO_3072 /* 2048-only */
+#else
+    /* Enable SP math all (sp_int.c) with multi-precision support */
+    #define WOLFSSL_SP_MATH_ALL
+#endif
+
+/* Enable hardening (timing resistance) */
 #define TFM_TIMING_RESISTANT
-
-#undef  ECC_TIMING_RESISTANT
 #define ECC_TIMING_RESISTANT
-
-#undef  WC_RSA_BLINDING
 #define WC_RSA_BLINDING
 
-#undef  HAVE_AESGCM
-#define HAVE_AESGCM
+/* Enable PRNG (SHA2-256) */
+#ifdef USE_LOW_RESOURCE
+    /* use TPM TRNG */
+    #define WC_NO_HASHDRBG
+#else
+    #define HAVE_HASHDRBG
+#endif
 
-#undef  WOLFSSL_SHA512
-#define WOLFSSL_SHA512
+/* Asymmetric */
+#if 1 /* RSA - needed to encrypt salt */
+    #undef  NO_RSA
+    #ifdef USE_LOW_RESOURCE
+        #define WOLFSSL_RSA_PUBLIC_ONLY
+        #define WOLFSSL_RSA_VERIFY_INLINE
+        #define NO_CHECK_PRIVATE_KEY
+    #endif
+#else
+    #define NO_RSA
+#endif
+#if 1 /* ECC - needed for encrypt ECC salt */
+    #define HAVE_ECC
+    #define ECC_USER_CURVES /* default to only SECP256R1 */
+#endif
+#ifndef USE_LOW_RESOURCE /* DH */
+    #undef  NO_DH
+    #define HAVE_FFDHE_2048
+    #define HAVE_DH_DEFAULT_PARAMS
+#else
+    #define NO_DH
+#endif
 
-#undef  WOLFSSL_SHA384
-#define WOLFSSL_SHA384
+/* Symmetric Hash */
+#undef NO_SHA
+#undef NO_SHA256
+#ifndef USE_LOW_RESOURCE
+    #define WOLFSSL_SHA512
+    #define WOLFSSL_SHA384
+#endif
 
-#undef  NO_DSA
-#define NO_DSA
+/* Symmetric Cipher */
+#define WOLFSSL_AES_CFB
+#define HAVE_AES_DECRYPT
+#ifndef USE_LOW_RESOURCE
+    #define HAVE_AES_KEYWRAP
+    #define WOLFSSL_AES_DIRECT
+    #define HAVE_AESGCM
+    #define GCM_TABLE_4BIT
+#else
+    #define NO_AES_CBC
+#endif
 
-#undef  HAVE_ECC
-#define HAVE_ECC
+#if 0 /* ChaCha20 / Poly1305 */
+    #define HAVE_POLY1305
+    #define HAVE_CHACHA
+#endif
 
-#undef  TFM_ECC256
-#define TFM_ECC256
+/* Features */
+#define WOLFSSL_ASN_TEMPLATE
+#define WOLFSSL_USER_IO /* user recv/send callbacks for network IO */
+#ifndef USE_LOW_RESOURCE
+    #define WOLFSSL_CERT_GEN
+    #define WOLFSSL_CERT_REQ
+    #define WOLFSSL_CERT_EXT
 
-#undef  WOLFSSL_BASE64_ENCODE
-#define WOLFSSL_BASE64_ENCODE
+    #define HAVE_PKCS7
+    #define HAVE_X963_KDF
+    #define WOLFSSL_BASE64_ENCODE
+#endif
 
-#undef  NO_RC4
-#define NO_RC4
-
-#undef  WOLFSSL_SHA224
-#define WOLFSSL_SHA224
-
-#undef  WOLFSSL_SHA3
-#define WOLFSSL_SHA3
-
-#undef  HAVE_POLY1305
-#define HAVE_POLY1305
-
-#undef  HAVE_ONE_TIME_AUTH
-#define HAVE_ONE_TIME_AUTH
-
-#undef  HAVE_CHACHA
-#define HAVE_CHACHA
-
-#undef  HAVE_HASHDRBG
-#define HAVE_HASHDRBG
-
-#undef  HAVE_TLS_EXTENSIONS
-#define HAVE_TLS_EXTENSIONS
-
-#undef  HAVE_SUPPORTED_CURVES
-#define HAVE_SUPPORTED_CURVES
-
-#undef  HAVE_EXTENDED_MASTER
-#define HAVE_EXTENDED_MASTER
-
-#undef  NO_PSK
-#define NO_PSK
-
-#undef  NO_MD4
-#define NO_MD4
-
-#undef  NO_PWDBASED
+/* Disables */
+#define NO_PKCS8
+#define NO_PKCS12
 #define NO_PWDBASED
-
-#undef  USE_FAST_MATH
-#define USE_FAST_MATH
-
-#undef  WOLFSSL_NO_ASM
-#define WOLFSSL_NO_ASM
-
-#undef  WOLFSSL_X86_BUILD
-#define WOLFSSL_X86_BUILD
-
-#undef  WC_NO_ASYNC_THREADING
-#define WC_NO_ASYNC_THREADING
-
-#undef  NO_DES3
+#define NO_DSA
 #define NO_DES3
+#define NO_RC4
+#define NO_PSK
+#define NO_MD4
+#define NO_MD5
+#define WOLFSSL_NO_SHAKE128
+#define WOLFSSL_NO_SHAKE256
+#define NO_WRITEV
 
-#undef  WOLFSSL_STATIC_MEMORY
-#define WOLFSSL_STATIC_MEMORY
-
-#undef  WOLFSSL_TLS13
-#define WOLFSSL_TLS13
-
-#undef  HAVE_HKDF
-#define HAVE_HKDF
-
-#undef  WC_RSA_PSS
-#define WC_RSA_PSS
-
-#undef  HAVE_FFDHE_2048
-#define HAVE_FFDHE_2048
+/* Low Resource Options */
+#ifdef USE_LOW_RESOURCE
+    #define NO_FILESYSTEM /* File system disable */
+    #define NO_ERROR_STRINGS
+    #define WOLFSSL_NO_ASM
+    #define TFM_NO_ASM
+    #define NO_WOLFSSL_MEMORY
+    #define NO_SESSION_CACHE
+    #define RSA_LOW_MEM
+    #define WOLFSSL_AES_SMALL_TABLES
+    #define WOLFSSL_AES_NO_UNROLL
+    #define GCM_SMALL
+    #undef  GCM_TABLE_4BIT
+    #define NO_AES_192
+    #define NO_AES_256
+    #define USE_SLOW_SHA
+    #define USE_SLOW_SHA256
+    #define USE_SLOW_SHA512
+    #define NO_SIG_WRAPPER
+    #define NO_ASN_TIME
+    #define NO_CODING
+    #define NO_BIG_INT
+#endif
 
 #ifdef __cplusplus
 }
