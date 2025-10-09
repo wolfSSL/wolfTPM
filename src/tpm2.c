9dd6c1f9928f4997920c617c28c8b67e6329e23d
Daniele Lacamera <root@danielinux.net>
2025-10-09 18:11:11 +0200

    Closing file descriptor on cleanup

--- src/tpm2.c
+++ src/tpm2.c
@@ -716,6 +716,11 @@ TPM_RC TPM2_Cleanup(TPM2_CTX* ctx)
     }
 #endif /* !WOLFTPM2_NO_WOLFCRYPT */
 
+#ifdef WOLFTPM_LINUX_DEV
+    if (ctx->fd >= 0)
+        close(ctx->fd);
+#endif
+
     return TPM_RC_SUCCESS;
 }
 
