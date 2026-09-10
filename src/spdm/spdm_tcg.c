e86c7573d0cf9d63dbcdcc534ff9c557f08cbdbb
Aidan Garske <aidan@wolfssl.com>
2026-09-10 11:16:55 -0700

    F-9179 - Reject a NULL vendor payload with a nonzero size

--- src/spdm/spdm_tcg.c
+++ src/spdm/spdm_tcg.c
@@ -204,6 +204,9 @@ int wolfSPDM_BuildVendorDefined(
     if (vdCode == NULL || outBuf == NULL) {
         return WOLFSPDM_E_INVALID_ARG;
     }
+    if (payload == NULL && payloadSz != 0) {
+        return WOLFSPDM_E_INVALID_ARG;
+    }
 
     /* SPDM VENDOR_DEFINED_REQUEST format:
      * SPDMVersion(1) + reqRspCode(1) + param1(1) + param2(1) +
