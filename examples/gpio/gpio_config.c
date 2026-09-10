27d1bf5f9f320ef2a48aaf7468e997d8c40a15b6
Aidan Garske <aidan@wolfssl.com>
2026-09-10 11:12:59 -0700

    F-10763 - Return an error on a GPIO config vendor mismatch

--- examples/gpio/gpio_config.c
+++ examples/gpio/gpio_config.c
@@ -176,6 +176,7 @@ int TPM2_GPIO_Config_Example(void* userCtx, int argc, char *argv[])
 #ifdef WOLFTPM_ST33
     if (caps.mfg != TPM_MFG_STM) {
         printf("TPM vendor mismatch. GPIO support requires an ST33 TPM 2.0 module\n");
+        rc = BAD_FUNC_ARG;
         goto exit;
     }
 
@@ -263,6 +264,7 @@ int TPM2_GPIO_Config_Example(void* userCtx, int argc, char *argv[])
 
     if (caps.mfg != TPM_MFG_NUVOTON) {
         printf("TPM vendor mismatch. GPIO support requires Nuvoton NPCT7xx TPM 2.0 module\n");
+        rc = BAD_FUNC_ARG;
         goto exit;
     }
 
